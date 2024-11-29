a6bad761d232d0c2b0aad3623d878959e55132fa
OmowunmiKamil <balikiskamil@gmail.com>
2024-11-29 05:09:42 +0100

    Add navigation button to BestSeller component and adjust styling

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,13 +1,20 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import ProductItem from "./ProductItem";
+import { useNavigate } from "react-router-dom";
 
 const BestSeller = () => {
   // Accessing the product data from ShopContext
   const { products } = useContext(ShopContext);
+  const navigate = useNavigate();
 
   // State to store the filtered best-selling products
   const [bestSellers, setBestSellers] = useState([]);
+
+  const navigateToShop = () => {
+    // Navigate to the shop page
+    navigate("/shop");
+  };
 
   useEffect(() => {
     // Filtering products marked as bestsellers
@@ -18,7 +25,7 @@ const BestSeller = () => {
   }, [products]); // Adding `products` as a dependency to run this effect when `products` changes
 
   return (
-    <div className="my-8">
+    <div className="my-10">
       <h2>Best Selling Items</h2>
 
       {/* Rendering Latest Collection Products */}
@@ -32,6 +39,15 @@ const BestSeller = () => {
             price={item.price}
           />
         ))}
+      </div>
+
+      <div className="flex justify-center my-8">
+        <button
+          onClick={navigateToShop}
+          className="text-xs md-text-sm text-white bg-gray-800 py-2 px-4 rounded-full hover:bg-gray-600"
+        >
+          GO TO SHOP
+        </button>
       </div>
     </div>
   );
