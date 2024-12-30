8984de0d02c7fdf7042b483ab004f9176a730648
OmowunmiKamil <balikiskamil@gmail.com>
2024-12-30 02:04:02 +0100

    Enhance Checkout component with billing address form and additional info section

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HerderBanner from "../components/HerderBanner";
+import CartTotal from "../components/CartTotal";
 
 const Checkout = () => {
   return (
@@ -12,45 +13,152 @@ const Checkout = () => {
       />
       {/* Checkout Form */}
       <div className="mt-20 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-        <div className="flex flex-col sm:flex-row justify-between gap-4">
+        <div className="flex flex-col sm:flex-row justify-between gap-8">
           {/* Billing Address */}
-          <div className="flex flex-col w-full sm:max-w-[650px] gap-6 sm:gap-8">
-            <h2 className="text-start my-0">Billing Address</h2>
+          <div className="flex flex-col w-full sm:max-w-[50%] gap-6">
+            <h2 className="text-start my-3">Billing Address</h2>
+
             {/* Form for entering billing address */}
             <div className="text-text">
-              <label htmlFor="fname">Firstname*</label>
+              <label htmlFor="fname" className="text-gray-500">
+                Firstname *
+              </label>
               <input
                 type="text"
                 name="fname"
                 className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
               />
             </div>
             <div className="text-text">
-              <label htmlFor="lname">Lastname*</label>
+              <label htmlFor="lname" className="text-gray-500">
+                Lastname *
+              </label>
               <input
                 type="text"
                 name="lname"
                 className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
               />
             </div>
             <div className="text-text">
-              <label htmlFor="companyname">Company name (optional)</label>
+              <label htmlFor="companyname" className="text-gray-500">
+                Company name (optional)
+              </label>
               <input
                 type="text"
                 name="companyname"
                 className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
               />
             </div>
             <div className="text-text">
-              <label htmlFor="conreg">Country / Region *</label>
+              <label htmlFor="conreg" className="text-gray-500">
+                Country / Region *
+              </label>
               <input
                 type="text"
                 name="conreg"
-                placeholder="United States (US"
+                placeholder="United States (US)"
                 className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="streetAdd" className="text-gray-500">
+                Street address *
+              </label>
+              <input
+                type="text"
+                name="streetAdd"
+                placeholder="House number and street name"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+              <input
+                type="text"
+                name="streetAdd"
+                placeholder="Apartments, suite, etc."
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="townCity" className="text-gray-500">
+                Town / City *
+              </label>
+              <input
+                type="text"
+                name="townCity"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="townCity" className="text-gray-500">
+                State *
+              </label>
+              <input
+                type="text"
+                name="townCity"
+                placeholder="Florida"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="townCity" className="text-gray-500">
+                Zip Code *
+              </label>
+              <input
+                type="number"
+                name="townCity"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="townCity" className="text-gray-500">
+                Phone *
+              </label>
+              <input
+                type="tel"
+                name="townCity"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
+              />
+            </div>
+            <div className="text-text">
+              <label htmlFor="townCity" className="text-gray-500">
+                Email address *
+              </label>
+              <input
+                type="email"
+                name="townCity"
+                className="w-full h-12 border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+                required
               />
             </div>
           </div>
+          {/* BAdditional Info */}
+          <div className="flex flex-col w-full sm:max-w-[50%] gap-6">
+            <h2 className="text-start my-3">Additional Info</h2>
+            <div className="text-text">
+              <label htmlFor="orderNotes" className="text-gray-500">
+                Order Notes (optional)
+              </label>
+              <textarea
+                name="orderNotes"
+                rows="1"
+                placeholder="Notes about your order. Like special notes for delivery."
+                className="w-full border border-checkBord bg-white rounded-lg shadow-md outline-none py-2 px-3 mt-2"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+        {/* Shipping Address */}
+        <div className="mt-8">
+          <CartTotal />
         </div>
       </div>
     </div>
